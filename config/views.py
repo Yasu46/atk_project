a58af84021595e52739075a0823705c6dc688fec
Yasu46 <y.suwa46@gmail.com>
2024-03-22 15:19:37 +0700

    create account app

---
+++ config/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib import messages
+
+
+def home(request):
+    return render(request, 'home.html')
